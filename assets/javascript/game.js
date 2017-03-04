007b711f4e8a8d85b19e4abf225e5bdd175de660
himapratap <himapratap@gmail.com>
2017-03-04 07:35:10 -0500

    Ok code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,61 +1,86 @@
 var dictionary = ["GERMANY", "AMERICA", "BRITAIN", "FRANCE", "ITALY"];
 var blanks = " ";
-var guesses = [];
+var guessArr = [];
 var guessesRemaining = 12;
 var guess;
 var answer = [];
 var isCompleted;
 var score = 0;
+var loses = 0;
 var song = "";
 var prevSong = "";
 
-var word = setNewWord();
+var computerGuess = setNewWord();
 setAnswerArray();
 
 document.onkeyup = function(event) {
     var valid = validateGuess(event.which);
     if (!valid) {
-        console.log("Invalid key typed" + event.key);
+        /*        console.log("Invalid key typed" + event.key);
+         */
         return;
     }
     guess = event.key.toUpperCase();
-    console.log("Valid key typed" + guess);
+    /*    console.log("Valid key typed" + guess);
+     */
+    if (guessesRemaining > 0) {
 
-    if (guessesRemaining >= 1) {
-        if (guesses.indexOf(guess) == -1) {
-            guesses.push(guess);
-            document.querySelector("#guessesRemaining").innerHTML = --guessesRemaining;
+        if (guessArr.indexOf(guess) == -1) {
 
-            document.querySelector("#guesses").innerHTML = guesses.toString();
-            if (word.search(guess) > -1) {
+            if (computerGuess.search(guess) > -1) {
                 fillTheBlank();
-                isCompleted = ifWon();
-                if (isCompleted) {
-                    score++;
-                    document.querySelector("#wins").innerHTML = score;
-                    document.querySelector("#result").innerHTML = "yaay u won !!";
-                    play();
+                success = ifWon();
+                if (success) {
+                    updateAndSetScore();
                     reset();
-
                 }
                 console.log("yay letter exist");
+            } else {
+                guessArr.push(guess);
+
             }
 
+            if (answer.indexOf(guess) == -1) {
+                document.querySelector("#guessesRemaining").innerHTML = --guessesRemaining;
+                document.querySelector("#guesses").innerHTML = guessArr;
+
+                if (guessesRemaining % 2 == 0) {
+                    document.querySelector("#man").src = "assets/images/man/" + "hang_" + (guessesRemaining) + ".gif"; //--guessesRemaining;
+
+                }
+
+            }
 
         }
-    } else {
-        document.querySelector("#result").innerHTML = "Oh u lost!! Better try this time";
-        console.log("lost !");
-        reset();
-
-
     }
 
-
-
+    if (guessesRemaining == 0) {
+        document.querySelector("#loses").innerHTML = ++loses;
+        document.querySelector("#result").innerHTML = "Oh u lost !!";
+        setTimeout(30000);
+        alert("U lost");
+        reset();
+    }
 
 }
 
+function updateAndSetScore() {
+    document.querySelector("#wins").innerHTML = ++score;
+    document.querySelector("#result").innerHTML = "yaay u won !!";
+    document.querySelector("#man").src = ""; //--guessesRemaining;
+
+    playMusic();
+    setFlag();
+}
+
+function setFlag() {
+    document.querySelector("#country").innerHTML = computerGuess; //--guessesRemaining;
+    document.querySelector("#help").innerHTML = "Press &uarr\;, &darr\; keys to increase / decrease sound"; //--guessesRemaining;
+
+    document.querySelector("#flag").src = "assets/images/" + computerGuess + ".png";
+
+
+}
 
 function validateGuess(charCode) {
     if ((charCode > 64 && charCode < 91) || (charCode > 96 && charCode < 123)) {
@@ -68,13 +93,14 @@ function validateGuess(charCode) {
 function reset() {
     guessesRemaining = 12;
     isCompleted = false;
-    guesses = [];
-    word = setNewWord();
+    guessArr = [];
+    computerGuess = setNewWord();
     setAnswerArray();
-    document.querySelector("#wins").innerHTML = score;
     document.querySelector("#guessesRemaining").innerHTML = guessesRemaining;
-    document.querySelector("#result").innerHTML = "";
-    document.querySelector("#guesses").innerHTML = "";
+    document.querySelector("#result").innerHTML = "  ";
+    document.querySelector("#guesses").innerHTML = "  ";
+    document.querySelector("#man").src = "assets/images/man/" + "hang_" + (guessesRemaining) + ".gif"; //--guessesRemaining;
+
 
 
 
@@ -88,15 +114,22 @@ function setNewWord() {
 
 function setAnswerArray() {
     answer = [];
-    for (var i = 0; i < word.length; i++) {
-        answer.push("_");
+    for (var i = 0; i < computerGuess.length; i++) {
+        console.log(computerGuess.charAt(i));
+        if (computerGuess.charAt(i) != ' ') {
+            answer.push("_");
+        } else {
+            answer.push(' ');
+        }
+
+
     }
     formatBlank();
 }
 
 function fillTheBlank() {
-    for (var i = 0; i < word.length; i++) {
-        if (word.charAt(i) == guess) {
+    for (var i = 0; i < computerGuess.length; i++) {
+        if (computerGuess.charAt(i) == guess) {
             answer[i] = guess;
         }
     }
@@ -107,8 +140,16 @@ function fillTheBlank() {
 function formatBlank() {
     var blanks = " ";
     for (var i = 0; i < answer.length; i++) {
-        blanks = blanks + " " + answer[i];
+        console.log("formatBlank", answer[i], "jhikjo");
+
+        if (answer[i] == " ") {
+            blanks = blanks + " " + "&nbsp";
+        } else {
+            blanks = blanks + " " + answer[i];
+
+        }
     }
+    console.log("blanks", blanks);
     document.querySelector("#currentWord").innerHTML = blanks;
 
 
@@ -121,26 +162,25 @@ function ifWon() {
 }
 
 
-function play() {
+function playMusic() {
     if (prevSong != "") {
         prevSong.pause();
 
     }
-    song = new Audio("assets/songs/" + word + ".mp3");
+    song = new Audio("assets/songs/" + computerGuess + ".mp3");
     song.currentTime = 0;
     song.play();
     song.volume = 0.2;
     prevSong = song;
-    document.querySelector("#flag").src = "assets/images/" + word + ".png";
 }
 
 
 document.onkeydown = function(event) {
-    if (prevSong != "" && prevSong.volume > 0 && prevSong.volume < 1) {
+    if (prevSong != "" && prevSong.volume > 0.02 && prevSong.volume < 1) {
         if (event.keyCode == 40) {
-            prevSong.volume = prevSong.volume - 0.05;
+            prevSong.volume = prevSong.volume - 0.02;
         } else if (event.keyCode == 38) {
-            prevSong.volume = prevSong.volume + 0.05;
+            prevSong.volume = prevSong.volume + 0.02;
 
         }
     }
